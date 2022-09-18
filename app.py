37060f3459b24b65e8fda55d4ad5432a3cf55865
Scott Neubauer <scotteneubauer@gmail.com>
2022-09-17 23:39:57 -0500

    Commit progress on scripts

--- app.py
+++ app.py
@@ -3,3 +3,12 @@ import scrape_mars
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 
+# # initialize the flask app
+# app = Flask(__name__)
+
+# # set up a connection to MongoDB
+# app.config["MONGO_URI"] = "mongodb://localhost:27017/mission_to_mars"
+# mongo = PyMongo(app)
+
+returned_data = scrape_mars.scrape()
+print(returned_data[0])
